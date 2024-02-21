b5da2a40a9fca99bc37e30c80ebcce375959b458
Matthieu THIBAUT <matthieu.thibaut@edu.devinci.fr>
2024-02-21 14:24:33 +0100

    [Code Example] Publish media

---
+++ examples/publish_media.py
@@ -0,0 +1,26 @@
+import os
+
+# Importing the necessary modules
+from pystagram.graph_api import PystagramGraphApi, ImageContainer
+
+# Initializing the PystagramGraphApi with the necessary credentials
+graph_api = PystagramGraphApi(
+    app_id=int(os.getenv("APP_ID")),  # The App ID from the environment variables
+    app_secret=os.getenv("APP_SECRET"),  # The App Secret from the environment variables
+    access_token=os.getenv("ACCESS_TOKEN"),  # The Access Token from the environment variables
+)
+
+# Creating an ImageContainer with the image URL and caption
+container = ImageContainer(
+    image_url="https://www.example.com/image.jpg",  # The URL of the image
+    caption="your caption #hashtag",  # The caption for the image
+    # Additional parameters can be added here
+)
+
+# Creating a media object with the ImageContainer
+response = graph_api.user.media.create(container)
+# Extracting the ID of the created media object
+container_id = response.data.get("id")
+
+# Publishing the created media object
+graph_api.user.media_publish.create(container_id=container_id)
